5e509ab2753df24f555cc034d7ef73077fabc0df
casserekevin <kevin_cassere13@hotmail.com>
2019-10-11 15:14:38 -0300

    Update Main.java

--- estatistica/src/measures/Main.java
+++ estatistica/src/measures/Main.java
@@ -9,7 +9,8 @@ public class Main {
 	public static void main(String[] args) {
 
 		int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
+		
+		//execution
 		Measures measures = new Measures();
 		measures.printAllMeasures(numbers);
 
